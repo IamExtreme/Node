0933398256f6f691762851c898da29b48fdb0a37
IamExtreme <luzsofiagn15@gmail.com>
2024-06-27 23:19:33 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// server.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,15 +8,25 @@ const PORT = process.env.PORT || 3000;
 // Middleware para parsear application/json
 app.use(bodyParser.json());
 
-// Ruta para recibir datos del dispositivo Arduino
+// Servir el archivo HTML
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para recibir datos del ESP32
 app.post('/recibir-datos', (req, res) => {
     const datos = req.body;
     console.log('Datos recibidos desde Arduino:', datos);
+    // Aquí puedes procesar los datos recibidos según sea necesario
+
+    // Emitir los datos a todos los clientes conectados
+    io.emit('nuevos-datos', datos);
+
     res.status(200).json({ mensaje: 'Datos recibidos correctamente' });
 });
 
 // Iniciar el servidor
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Servidor escuchando en puerto ${PORT}`);
 });
 
+// Configurar WebSockets
+const io = require('socket.io')(server);
